737fad3b323e7274e79d4cc814816241ea9a847c
Joshua <yoshiserry@gmail.com>
2018-05-14 22:24:35 +1000

    updated config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'l\xbb|N\xa3@j\xf6\x06\x10\x8fU\x86\xa1\xe5\xdf\x04\x15D?B\xe0KB' #os.urandom(24)
     #update from database name hardcoded to Database_URL environment variable.
-    # Then update the environment variable in the virtualenv Flask Environment via terminal.
+    # Then update the environment variable in the virtualenv Flask Environment via terminal with export command.
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
