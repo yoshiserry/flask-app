26a8a1478e5919a6a30b60a1a5e17ddfdb308a92
Joshua <yoshiserry@gmail.com>
2018-05-14 16:26:38 +1000

    updated config

--- app.py
+++ app.py
@@ -35,7 +35,11 @@ def login():
     return render_template('login.html', error=error)
 
 #run the app on localhost port 5000
-if __name__ == '__main__':
+#if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 8000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+#    port = int(os.environ.get('PORT', 8000))
+#    app.run(debug=False, host='0.0.0.0', port=port)
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+app.run()
